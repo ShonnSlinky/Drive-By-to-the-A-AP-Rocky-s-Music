8e21e306e6fecac96c27d67759dad9ac454feaf3
ShoneSlinky <ihavemyselfinbafroom@gmail.com>
2016-10-01 23:16:43 +0300

    Ui update two

---
+++ Assets/scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+    [SerializeField]
+    GameObject Radio;
+    [SerializeField]
+    GameObject Back;
+    [SerializeField]
+    GameObject Menu;
+    // Use this for initialization
+    void Start () {
+        Radio.SetActive(false);
+	}
+	
+    public void OnPoupMenuOpen()
+    {
+        Radio.SetActive(true);
+    }
+
+    private void OnPoupMenuClose()
+    {
+        Radio.SetActive(false);
+    }
+    private void OnMenuOpen()
+    {
+
+    }
+}
